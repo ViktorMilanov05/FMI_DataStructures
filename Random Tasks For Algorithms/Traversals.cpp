10e33e67ba572d02ad9f8c7a10d603adc80c59c4
Viktor Milanov <101833400+ViktorMilanov@users.noreply.github.com>
2025-01-09 20:51:55 +0200

    Update Traversals.cpp

--- Random Tasks For Algorithms/Traversals.cpp
+++ Random Tasks For Algorithms/Traversals.cpp
@@ -18,13 +18,27 @@ public:
 	int shortestPath(size_t start, size_t end) const;
 	int shortestPathWithVector(size_t start, size_t end) const;
 
-	void dfsIter(size_t start);
-	void dfsRec(size_t start);
+	void dfsIter(size_t start) const;
+	void dfsRec(size_t start) const;
 	bool containsCycle() const;
+
+	size_t countConnectedComponets() const;
+	std::vector<int> topologicalSort(size_t start) const;
+	static struct Edge {
+		int vertex;
+		int weight;
+		Edge(int vertex, int weight) : vertex(vertex), weight(weight) {};
+		bool operator<(const Edge& other) const{
+			return weight > other.weight;
+		}
+	};
+	std::vector<int> diikstra(std::vector<std::vector<std::pair<int, int>>>,size_t start) const; //only for gragh with weight
 private:
 	std::vector<std::vector<int>> adj;
 	bool isOriented;
 
+	void topologicalSortHelper(std::vector<bool>& visited, std::vector<int>& topSort, size_t start) const;
+	void dfsForCountConnectedComponets(std::vector<bool>& visited, size_t start) const;
 	void dfsRecHelper(std::vector<bool>& visited, size_t start) const;
 	bool containsCycleHelper(std::vector<bool>& visited, std::vector<bool>& inStack, size_t start) const;
 };
@@ -39,7 +53,7 @@ void Graph::bfs(size_t start) const
 		size_t curr = q.front();
 		q.pop();
 		std::cout << curr << ' ';
-		for (auto neighbor : adj[curr]) {
+		for (const auto& neighbor : adj[curr]) {
 			if (visited[neighbor])
 				continue;
 			q.push(neighbor);
@@ -64,7 +78,7 @@ int Graph::shortestPath(size_t start, size_t end) const
 		{
 			size_t curr = q.front();
 			q.pop();
-			for (auto neighbor : adj[curr]) {
+			for (const auto& neighbor : adj[curr]) {
 				if (visited[neighbor])
 					continue;
 				if (neighbor == end) {
@@ -92,7 +106,7 @@ int Graph::shortestPathWithVector(size_t start, size_t end) const
 		std::vector<size_t> nextLevel;
 		for (size_t el : currLevel)
 		{
-			for (auto neighbor : adj[el])
+			for (const auto& neighbor : adj[el])
 			{
 				if (visited[neighbor])
 					continue;
@@ -109,7 +123,7 @@ int Graph::shortestPathWithVector(size_t start, size_t end) const
 	return -1;
 }
 
-void Graph::dfsIter(size_t start)
+void Graph::dfsIter(size_t start) const
 {
 	std::vector<bool> visited(adj.size(), false);
 	std::stack<size_t> s;
@@ -121,13 +135,13 @@ void Graph::dfsIter(size_t start)
 			continue;
 		visited[curr] == true;
 		std::cout << curr << ' ';
-		for (auto neighbor : adj[curr]) {
+		for (const auto& neighbor : adj[curr]) {
 			s.push(neighbor);
 		}
 	}
 }
 
-void Graph::dfsRec(size_t start)
+void Graph::dfsRec(size_t start) const
 {
 	std::vector<bool> visited(adj.size(), false);
 	dfsRecHelper(visited, start);
@@ -146,15 +160,83 @@ bool Graph::containsCycle() const
 	return false;
 }
 
-void Graph::dfsRecHelper(std::vector<bool>& visited, size_t start) const
+size_t Graph::countConnectedComponets() const
 {
-		visited[start] == true;
-		std::cout << start << ' ';
-		for (auto neighbor : adj[start]) {
-			if (!visited[start]) {
-				dfsRecHelper(visited, neighbor);
+	int count = 0;
+	std::vector<bool> visited(adj.size(), false);
+	for (size_t i = 0; i < adj.size(); i++)
+	{
+		if (!visited[i]) {
+			dfsForCountConnectedComponets(visited, i);
+			count++;
+		}
+	}
+	return count;
+}
+
+std::vector<int> Graph::topologicalSort(size_t start) const
+{
+	std::vector<bool> visited(adj.size(), false);
+	std::vector<int> result;
+	topologicalSortHelper(visited, result, start);
+	std::reverse(result.begin(), result.end());
+	return result;
+}
+
+std::vector<int> Graph::diikstra(std::vector<std::vector<std::pair<int, int>>> adjs, size_t start) const
+{
+	std::vector<bool> visited(adjs.size(), false);
+	std::vector<int> distance(adjs.size(), INT_MAX);
+	std::priority_queue<Edge> pq;
+	pq.push({ start, 0 });
+	distance[start] = 0;
+	while (!pq.empty()) {
+		int currVertex = pq.top().vertex;
+		int currWeight = pq.top().weight;
+		pq.pop();
+		if (visited[currVertex])
+			continue;
+		visited[start] = true;
+		for (const auto& neighbor : adjs[currVertex]) {
+			if (neighbor.second + currWeight < distance[neighbor.first]) {
+				distance[neighbor.first] = neighbor.second + currWeight;
+				pq.push({ neighbor.first, distance[neighbor.first] });
 			}
 		}
+	}
+	return distance;
+}
+
+void Graph::topologicalSortHelper(std::vector<bool>& visited, std::vector<int>& topSort, size_t start) const
+{
+	visited[start] = true;
+	for (const auto& neighbor : adj[start]) {
+		if (!visited[neighbor]) {
+			topologicalSortHelper(visited, topSort, neighbor);
+		}
+	}
+	topSort.push_back(start);
+}
+
+void Graph::dfsForCountConnectedComponets(std::vector<bool>& visited, size_t start) const
+{
+	visited[start] = true;
+	for (const auto& neighbor : adj[start])
+	{
+		if (!visited[neighbor])
+			dfsForCountConnectedComponets(visited, neighbor);
+	}
+}
+
+void Graph::dfsRecHelper(std::vector<bool>& visited, size_t start) const
+{
+	visited[start] == true;
+	std::cout << start << ' ';
+	for (const auto& neighbor : adj[start]) {
+		if (!visited[start]) {
+			dfsRecHelper(visited, neighbor);
+		}
+	}
 }
 
 bool Graph::containsCycleHelper(std::vector<bool>& visited, std::vector<bool>& inStack, size_t start) const
@@ -162,7 +244,7 @@ bool Graph::containsCycleHelper(std::vector<bool>& visited, std::vector<bool>& i
 	if (!visited[start]) {
 		visited[start] = true;
 		inStack[start] = true;
-		for (auto neighbor : adj[start])
+		for (const auto& neighbor : adj[start])
 		{
 			if (!visited[neighbor] && containsCycleHelper(visited, inStack, neighbor)) {
 				return true;
